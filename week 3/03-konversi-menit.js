bf9057d2e6982e11e5fc7b545b47290e08fc5594
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-13 13:17:27 +0700

    Update 03-konversi-menit.js

--- week 3/03-konversi-menit.js
+++ week 3/03-konversi-menit.js
@@ -6,7 +6,9 @@ Exercise Konversi Menit
 Logic Challenge - Konversi Menit
 
 Problem
-Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu dalam menit. Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. Contoh, jika menit adalah 63, maka function akan me-return "1:03".
+Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter berupa angka yang merupakan ukuran waktu dalam menit. 
+Function akan me-return string waktu dalam format jam:menit berdasarkan menit tersebut. 
+Contoh, jika menit adalah 63, maka function akan me-return "1:03".
 */
 
 function konversimenit(num){
