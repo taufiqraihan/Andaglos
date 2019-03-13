b2d59c0dafb54992509bfa6f2253ba2d3d56ebf8
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-13 13:12:57 +0700

    Update 01-banding-angka.js

--- week 3/01-banding-angka.js
+++ week 3/01-banding-angka.js
@@ -1,3 +1,16 @@
+/*
+Exercise Bandingkan Angka 
+
+
+
+Logic Challenge - Bandingkan Angka
+
+Problem
+Diberikan sebuah function bandingkanAngka(angka1, angka2) yang menerima dua parameter angka.
+Function akan me-return nilai true jika angka2 lebih besar dari angka1, dan false jika sebaliknya. 
+Jika kedua angka bernilai sama, function akan me-return -1.
+*/
+
 function bandingAngka(angka1, angka2){
 if(angka2 > angka1){
 	return true;
