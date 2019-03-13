965bd596ca740feb4efaba526df0dda3add47c5c
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-13 13:16:11 +0700

    Update 02-balik-kata.js

--- week 3/02-balik-kata.js
+++ week 3/02-balik-kata.js
@@ -1,3 +1,14 @@
+/*
+Exercise Balik kata 
+
+
+
+Logic Challenge - Balik Kata
+
+Problem
+Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string. 
+Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ"
+*/
 function balikKata(kata){
   let kebalik= '';
   for(i = kata.length-1; i>=0 ; i--){
