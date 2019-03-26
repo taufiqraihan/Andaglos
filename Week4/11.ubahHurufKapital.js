3559731405dbd2055f02b80d618384c7115f48f6
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-26 14:37:18 +0700

    Update 11.ubahHurufKapital.js

--- Week4/11.ubahHurufKapital.js
+++ Week4/11.ubahHurufKapital.js
@@ -1,3 +1,24 @@
+/*
+Logic Challenge - Ubah Kapital
+Problem
+Diberikan function ubahKapital(kalimat) yang menerima satu parameter berupa string.
+Function akan me-return nilai berupa kalimat dimana setiap kata dimulai dengan huruf besar.
+Contoh, jika input adalah "saya makan sushi", maka output adalah "Saya Makan Sushi".
+
+Code
+*/
+//1.0 inisialisasi
+  //1.1 pisahkan kata kalimt dengan spasi dan masukan ke variable splitKalimat
+  //1.2 buat variable penampung kalimatBaru bentuk string
+//2.0 membaca kata kalimat yang sudah dipisahkn dengan buat pengulangan
+  //2.1 mengecek huruf pertama di dalam kata kalimat
+    //2.1.1 jika huruf pertama pada kata kalimat  ditemukan maka
+      // kalimat baru di tambahkan kata dari kalimat dengan huruf pertamanya diganti kapital
+    //2.1.2 jika huruf selanjutny selain huruf pertama pada kata kalimat di temukan maka
+      // kalimat baru ditambahkn kata dari kalimat dengan huruf kecil
+//3.0 return kalimat baru dengan memotong spasi diawal dan akhir
+
+//code disini
 function ubahKapital(kalimat) {
   var splitKalimat = kalimat.split(' ');
   var kalimatBaru ='';
@@ -13,8 +34,6 @@ function ubahKapital(kalimat) {
   }
   return kalimatBaru.trim();
 }
-
-
 
 // TEST CASES
 console.log(ubahKapital('saya makan sushi')); // Saya Makan Sushi
