62ddaf17ee35bbc09656a77fdbb77d065380279d
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-26 14:32:47 +0700

    Update 7.mengurutkanAbjad.js

--- Week4/7.mengurutkanAbjad.js
+++ Week4/7.mengurutkanAbjad.js
@@ -1,9 +1,22 @@
+/*
+Logic Challenge - Mengurutkan Abjad
+Problem
+Diberikan sebuah function urutkanAbjad(str) yang menerima satu parameter berupa string.
+Function tersebut akan mengembalikan string baru yang telah diubah urutan abjadnya dari a hingga z.
+Contohnya, halo akan menjadi ahlo.
+Kamu tidak perlu memikirkan apabila ada simbol ataupun angka didalam string tersebut.
+
+Code
+*/
+//1.0 memisahkan kata string
+//1.1 mengurutkn hasil pemishan kata string
+//1.2 menyatukan semua huruf string yang sudah diurutkan menjadi kata baru
+//2.0 return kata tersebut
+
+//code disini
 function urutkanAbjad(str) {
-    var result=str.split("").sort().join("");
-    return result;
+  return str.split('').sort('').join('');
 }
-
-
 
 // TEST CASES
 
