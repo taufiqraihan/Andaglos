e7c31ad4bff26b6ef0b83fda60aa7600a3edb5a3
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-26 14:37:52 +0700

    Update 12.ubahHuruf.js

--- Week4/12.ubahHuruf.js
+++ Week4/12.ubahHuruf.js
@@ -1,3 +1,25 @@
+/*Logic Challenge - Ubah Huruf
+Problem
+Diberikan function ubahHuruf(kata) yang akan menerima satu parameter berupa string.
+Functiona akan me-return sebuah kata baru dimana setiap huruf akan digantikan dengan huruf alfabet setelahnya.
+Contoh, huruf a akan menjadi b, c akan menjadi d, k menjadi l, dan z menjadi a.
+
+Code
+*/
+//1.0 inisialisasi
+  //1.1 buat variable kamus huruf bentuk string dari a-z
+  //1.2 lalu isi variable kamus huruf di pisahkan satu-satu huruf dan masukkan ke variable kamusHurufArr
+  //1.3 buat variable tampung kataBaru bentuk string
+//2.0 buat pengulangan untuk pengecekan dan membaca dari kiri ke kanan parameter kata sepanjng kata tersebut
+  //2.1 buat pengulangan lagi untuk membaca kamus huruf array dari kiri ke kanan sepanjang array
+    //2.1.0 jika ditemukan huruf di index kata sama dengan huruf di kamus array maka
+      //2.1.2 jika ditemukan huruf kamus array sama dengan z maka
+      // kata baru di ganti huruf a
+      //2.1.3 jika tidak ditemukan kesamaan di atas
+      // maka kata bari ditambahkan huruf berikutnya
+//3.0 return kata baru
+
+//code disini
 function ubahHuruf(kata) {
   var kamusHuruf = 'abcdefghijklmnopqrstuvwxyz';
   var kamusHurufArr = (kamusHuruf.split(''));
@@ -16,8 +38,6 @@ function ubahHuruf(kata) {
   }
   return kataBaru;
 }
-
-
 
 // TEST CASES
 console.log(ubahHuruf('wow')); // xpx
