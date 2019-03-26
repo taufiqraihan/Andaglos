a71386cb940a1404acbbb49250723a25be500645
MUHAMMAD TAUFIQ RAIHAN <taufiqraihan9mn@gmail.com>
2019-03-26 14:36:49 +0700

    Update 10.tambahDashAtauStrip.js

--- Week4/10.tambahDashAtauStrip.js
+++ Week4/10.tambahDashAtauStrip.js
@@ -1,3 +1,26 @@
+/*
+Logic Challenge - Tambah Dash / Strip
+Problem
+Diberikan sebuah function tambahDash(str) yang menerima satu parameter berupa string.
+Function akan mengembalikan str tersebut dengan menambahkan simbol dash ('-') di antara dua angka ganjil di dalam string yang menjadi parameter tersebut.
+Contoh, jika str adalah 1233897, function akan mengembalikan nilai '123-389-7'. 0 bukanlah angka ganjil.
+
+Code
+*/
+//1.0 inisialisasi
+  //1.1 memisahkan parameter str dengan spasi lalu tampung di variable splitStr
+  //1.2 buat variable strBaru untuk menampung hasil
+//2.0 buat perulangn untuk mengecek angka berupa genap atau ganjil
+  //2.1 jika sebuah angka berupa genap maka
+      // tampung di strBaru angka- angka yang displit
+  //2.2 jika bukan genap maka
+    //2.2.1 cek jika ganjil maka
+        // tampung di strBaru angka- angka yang displit dengan tambahan string -
+    //2.2.2 jika ternyta genap maka
+        // tampung di strBaru angka- angka yang displit
+//3.0 return hasil strBaru
+
+//code disini
 function tambahDash(str) {
   var splitStr = str.split('');
   var strBaru = '';
@@ -17,8 +40,6 @@ function tambahDash(str) {
   }
   return strBaru;
 }
-
-
 
 // TEST CASES
 console.log(tambahDash('454793')); // '4547-9-3'
